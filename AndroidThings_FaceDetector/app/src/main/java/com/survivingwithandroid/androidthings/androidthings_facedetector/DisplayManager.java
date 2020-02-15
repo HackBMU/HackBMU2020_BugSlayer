33548403376534eba0cb22ef424f33f2ef6e2e88
ishanvohra2 <ishanvohra2@gmail.com>
2020-02-15 21:56:11 +0530

    First commit

---
+++ AndroidThings_FaceDetector/app/src/main/java/com/survivingwithandroid/androidthings/androidthings_facedetector/DisplayManager.java
@@ -0,0 +1,44 @@
+package com.survivingwithandroid.androidthings.androidthings_facedetector;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.google.android.things.contrib.driver.ssd1306.BitmapHelper;
+import com.google.android.things.contrib.driver.ssd1306.Ssd1306;
+import java.io.IOException;
+
+public class DisplayManager {
+
+    private static final String TAG = DisplayManager.class.getName();
+
+    private Ssd1306 display;
+
+    public DisplayManager() {}
+
+    public void init() {
+        try {
+            display = new Ssd1306("I2C1");
+            display.setContrast(100);
+            Log.d(TAG, "Display initialized!");
+        }
+        catch(IOException ioe) {
+            Log.e(TAG, "Error initialing the display");
+            ioe.printStackTrace();
+        }
+    }
+
+    public void setImage(Resources res, int resId) {
+        display.clearPixels();
+        Bitmap bmp = BitmapFactory.decodeResource(res, resId);
+        BitmapHelper.setBmpData(display, 0,0, bmp, true);
+
+        try {
+            display.show();
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
---
+++ AndroidThings_FaceDetector/app/src/main/java/com/survivingwithandroid/androidthings/androidthings_facedetector/MainActivity.java
@@ -0,0 +1,59 @@
+package com.survivingwithandroid.androidthings.androidthings_facedetector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = FaceDetector.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView) findViewById(R.id.img);
+
+        final DisplayManager display = new DisplayManager();
+        display.init();
+        display.setImage(getResources(), R.drawable.neutral_face);
+
+        FirebaseApp.initializeApp(this);
+        FaceDetector fc = new FaceDetector(this, img, getMainLooper());
+        fc.setListener(new FaceDetector.CameraListener() {
+            @Override
+            public void onError() {
+                // Handle error
+            }
+
+            @Override
+            public void onSuccess(FaceDetector.FACE_STATUS status) {
+                Log.d(TAG, "Face ["+status+"]");
+
+                switch (status) {
+                    case SMILING:
+                       display.setImage(getResources(), R.drawable.smiling_face);
+                       Toast.makeText(MainActivity.this, "smiling", Toast.LENGTH_SHORT).show();
+                        break;
+                    case LEFT_EYE_OPEN_RIGHT_CLOSE:
+                        display.setImage(getResources(), R.drawable.right_eyes_closed);
+                        Toast.makeText(MainActivity.this, "Right eye close", Toast.LENGTH_SHORT).show();
+                        break;
+                    case RIGHT_EYE_OPEN_LEFT_CLOSE:
+                        display.setImage(getResources(), R.drawable.left_eyes_closed);
+                        Toast.makeText(MainActivity.this, "Left eye close", Toast.LENGTH_SHORT).show();
+                        break;
+                     default:
+                        display.setImage(getResources(), R.drawable.neutral_face);
+                }
+
+            }
+        });
+
+        fc.capture();
+    }
+}
